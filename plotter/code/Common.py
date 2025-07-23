ce6f72e17212f8b6eed95d21c07ffc97240a1bf5
matteSata <msatanassi02@gmail.com>
2025-07-23 14:07:31 +0200

    Modificata la classe Exp e aggiunto il metodo fill() Aggiunto il campo name alla classe Curve Riscritta la definizione della classe ExpCurves Aggiunta la classe ExpPlots, con il metodo plot() Aggiunta la classe di paramatri CP (Curve Parameters)

--- plotter/code/Common.py
+++ plotter/code/Common.py
@@ -7,16 +7,23 @@ import pandas as pd
 
 # identifica un esperimento, a cui appartengono 4 curve
 class Exp:
-    def __init__(self, trap_distr:str, e_sigma:float, e_mid:float, v_gf:int, file_path:Path):
+    def __init__(self, file_path:Path, trap_distr:str=None, e_sigma:float=None, e_mid:float=None, v_gf:int=None) -> None:
         self.trap_distr: str = trap_distr
         self.Es: float = e_sigma
         self.Em: float = e_mid
         self.Vgf: int = v_gf
         self.path:Path = file_path
+    def fill(self)->None:
+        info = np.array((self.path.stem.split('_')))  # es: IdVd_exponential_Vgf_2_Es_1.72_Em_1.04
+        self.trap_distr = info[1]
+        self.Es = float(info[5])
+        self.Em = float(info[7])
+        self.Vgf = int(info[3])
 
 # identifica una singola curva di un esperimento
 class Curve:
-    def __init__(self):
+    def __init__(self, name:str):
+        self.name:str = name
         self.X: np.ndarray = None
         self.Y: np.ndarray = None
     def sort(self)->None:   #ordina gli array delle coordinate in modo crescente, rispetto alle x
@@ -31,12 +38,9 @@ class Curve:
 class ExpCurves:
     def __init__(self, exp:Exp):
         self.exp:Exp = exp
-        self.curves:dict[str, Curve] = {
-            'v0': Curve(),
-            '0': Curve(),
-            '15': Curve(),
-            '30': Curve(),
-        }
+        self.curves:dict[str, Curve] = dict()
+        for key, name in CP.names.items():
+            self.curves[key] = Curve(name)
     def sort(self):
         self.curves['v0'].sort()
         self.curves['0'].sort()
@@ -69,6 +73,30 @@ class ExpCurves:
             y_maxs.append(y_max)
         return min(y_mins), max(y_maxs)
 
+class ExpPlots:
+    def __init__(self, exp:Exp):        #possibilità di aggiungere più esperimenti in array
+        self.exp_curves = ExpCurves(exp)
+        self.fig = go.Figure()
+    def plot(self, c_to_plot:list[str])->None:
+        self.exp_curves.import_csv()
+        self.exp_curves.sort()
+        for c in c_to_plot:
+            self.fig.add_trace(go.Scatter(
+                x=self.exp_curves.curves[c].X,
+                y=self.exp_curves.curves[c].Y,
+                name=self.exp_curves.curves[c].name,
+                mode='lines+markers',
+                line=dict(
+                    color=CP.colors[c] if Config.colors else 'black',
+                    dash=None if Config.colors else CP.linestyles[c],
+                ),
+                marker=dict(
+                    symbol='square',
+                ),
+                visible=True
+            ))
+        return None
+
 ## PARAMS ##
 class Config:
     DPI = 300
@@ -78,3 +106,23 @@ class Config:
     same_fig = True
     legend = True
     colors = True
+
+class CP:   # curves params
+    names = {
+        "v0": "(0,0)",
+        "0": "(-7,0)",
+        "15": "(-7,15)",
+        "30": "(-7,30)"
+    }
+    colors = {
+        "v0": "red",
+        "0": "limegreen",
+        "15": "dodgerblue",
+        "30": "darkorange"
+    }
+    linestyles = {
+        "v0": "dashdot",
+        "0": "dotted",
+        "15": "dashed",
+        "30": "solid"
+    }   # to use in case of colorless image configuration
