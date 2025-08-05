06e75fd92fbae2169171134ab3c709727b0b4484
matteSata <msatanassi02@gmail.com>
2025-08-05 03:34:38 +0200

    Nascosto il bottone toggle_column nella tabella, aggiunto il bottone per l'esportazione dei grafici selezionati Ristretta la callback update_graph_content() Migliorata la callback update_table() Aggiunta la callback export_selected()

--- plotter/code/IdVd_page.py
+++ plotter/code/IdVd_page.py
@@ -39,46 +39,51 @@ app.layout = dbc.Container(
                         ),
                         # html.Div(id='table')
                         dash_table.DataTable(
-                                    id='table',
-                                    data=exp_mode_dict,
-                                    columns=[
-                                        {'name': 'Trap Distribution', 'id': 'trap_distr'},
-                                        {'name': "E_mid", 'id': 'e_mid'},
-                                        {'name': 'E_σ', 'id': 'e_sigma'},
-                                        {'name': 'V_gf', 'id': 'v_gf'},
-                                        {'name': 'file_path', 'id': 'file_path'},
-                                        {'name': 'group', 'id': 'group'},
-                                    ],
-                                    sort_action='native',
-                                    filter_action='native',
-                                    filter_options={"placeholder_text": "Filter column..."},
-                                    row_selectable='multi',
-                                    selected_rows=[],
-                                    hidden_columns=['file_path', 'group'],
-                                    page_size=10,
-                                    style_cell={'textAlign': 'right'},
-                                    style_cell_conditional=[
-                                        {
-                                            'if': {'column_id': 'trap_distr'},
-                                            'textAlign': 'left'
-                                        }
-                                    ],
-                                    style_data={
-                                        'color': 'black',
-                                        'backgroundColor': 'white'
-                                    },
-                                    style_data_conditional=[
-                                        {
-                                            'if': {'row_index': 'odd'},
-                                            'backgroundColor': 'rgb(220, 220, 220)',
-                                        }
-                                    ],
-                                    style_header={
-                                        'backgroundColor': 'rgb(210, 210, 210)',
-                                        'color': 'black',
-                                        'fontWeight': 'bold'
-                                    }
-                                )
+                            id='table',
+                            data=exp_mode_dict,
+                            columns=[
+                                {'name': 'Trap Distribution', 'id': 'trap_distr'},
+                                {'name': "E_mid", 'id': 'e_mid'},
+                                {'name': 'E_σ', 'id': 'e_sigma'},
+                                {'name': 'V_gf', 'id': 'v_gf'},
+                                {'name': 'file_path', 'id': 'file_path'},
+                                {'name': 'group', 'id': 'group'},
+                            ],
+                            sort_action='native',
+                            filter_action='native',
+                            filter_options={"placeholder_text": "Filter column..."},
+                            row_selectable='multi',
+                            selected_rows=[],
+                            hidden_columns=['file_path', 'group'],
+                            page_size=10,
+                            style_cell={'textAlign': 'right'},
+                            style_cell_conditional=[
+                                {
+                                    'if': {'column_id': 'trap_distr'},
+                                    'textAlign': 'left'
+                                }
+                            ],
+                            style_data={
+                                'color': 'black',
+                                'backgroundColor': 'white'
+                            },
+                            style_data_conditional=[
+                                {
+                                    'if': {'row_index': 'odd'},
+                                    'backgroundColor': 'rgb(220, 220, 220)',
+                                }
+                            ],
+                            style_header={
+                                'backgroundColor': 'rgb(210, 210, 210)',
+                                'color': 'black',
+                                'fontWeight': 'bold'
+                            },
+                            css=[{"selector": ".show-hide", "rule": "display: none"}]
+                        ),
+                        dbc.Button("Export selected!",
+                                   id='export-button',
+                                   n_clicks=0,
+                                   style={'textAlign': 'left'}),
                     ],
                     width=4,    #larghezza colonna
                     style={'textAlign': 'center'},
@@ -131,7 +136,7 @@ app.layout = dbc.Container(
     State('table','derived_virtual_selected_rows'), #selected_rows: indici delle righe selezionate (considerati filtri vari colonne)
     State('table','derived_virtual_data'),      #table_data: dati della tabella (considerati filtri vari colonne)
     State('tabs','value'),                      #curr_tab: tab attualmente aperto (se nessuno None)
-    State('tabs','children')                    #tabs: lista dei tab disponibili
+    State('tabs','children'),                   #tabs: lista dei tab disponibili
 )
 def update_tabs(n_clicks, curr_mode, selected_rows, table_data, curr_tab, tabs):
     labels = {'exponential':'exp', 'gaussian':'gauss', 'uniform':'unif'}
@@ -162,7 +167,6 @@ def update_tabs(n_clicks, curr_mode, selected_rows, table_data, curr_tab, tabs):
                     )
                 )
 
-
     if not open_tabs:
         if curr_mode == 'Exp mode':
             return tabs,[],table_data[selected_rows[0]]['file_path']    # se non c'è alcun tab già aperto, apro il primo tab
@@ -185,27 +189,58 @@ def update_graph_content(tab, checked_curves):
         g = Group()
         for file in df_group['file_path']:
             g.add_path(Path(file))
-        g_plot = GroupPlot(g)
-        g_plot.plot(checked_curves)
+        g_plot = GroupPlot(g).plot(checked_curves)
         return dcc.Graph(figure=g_plot.fig)
     else:
-        exp = Exp(Path(tab))
-        exp.fill()
-        exp_plot = ExpPlot(exp)
-        exp_plot.plot(checked_curves)
+        exp = Exp(tab).fill()
+        exp_plot = ExpPlot(exp).plot(checked_curves)
         return dcc.Graph(figure=exp_plot.fig)
 
 # aggiorna la tabella in base alla modalità selezionata
 @callback(
     Output('table', 'data'),
     Output('table', 'hidden_columns'),
-    Input('group-mode-toggle', 'value')
+    Output('table', 'selected_rows',allow_duplicate=True),
+    Input('group-mode-toggle', 'value'),
+    prevent_initial_call=True
 )
 def update_table(mode):
     if mode == 'Exp mode':
-        return exp_mode_dict,['file_path', 'group']
+        return exp_mode_dict,['file_path', 'group'],[]
     else:
-        return group_mode_dict,['file_path', 'group', 'v_gf']
+        return group_mode_dict,['file_path', 'group', 'v_gf'],[]
+
+@callback(
+    Output('table', 'selected_rows', allow_duplicate=True),
+    Input('export-button', 'n_clicks'),
+    State('group-mode-toggle', 'value'),
+    State('table', 'derived_virtual_selected_rows'),
+    State('table', 'derived_virtual_data'),
+    prevent_initial_call=True
+)
+def export_selected(n_clicks, mode, selected_rows, data_table):     # AGGIUNGERE BOX PER SCELTA DOWNLOAD_PATH, ESTENSIONE, CURVE DA PLOTTARE
+    if not n_clicks or not selected_rows:
+        return selected_rows
+    export_path = find_export_path()
+    figs,exp_file_paths = [],[]
+    match mode:
+        case 'Exp mode':
+            exps:list[Exp] = [Exp(data_table[row_i]['file_path']).fill() for row_i in selected_rows]        # lista di esperimenti corrispondente alle righe selezionate
+            plots:list[ExpPlot] = [ExpPlot(exp).plot(['v0','0','15','30']) for exp in exps]                 # lista di ExpPlot corrispondente
+            figs:list[go.Figure] = [plot.fig for plot in plots]
+            exp_file_paths:list[Path] = [export_path/Path(f"{plot.exp_curves.exp}.png") for plot in plots]  #estensioni possibili .png, .svg, .pdf
+        case 'Group mode':
+            groups_files:list[list[str]] = [df.loc[df.group==data_table[row_i]['group']]['file_path'].tolist() for row_i in selected_rows]
+            groups:list[Group] = [Group().add_paths(group_files) for group_files in groups_files]
+            plots:list[GroupPlot] = [GroupPlot(g).plot(['v0','0','15','30']) for g in groups]
+            figs:list[go.Figure] = [plot.fig for plot in plots]
+            exp_file_paths:list[Path] = [export_path/Path(f"{plot.group_curves.group}.png") for plot in plots]
+    pio.write_images(
+        fig=figs,
+        file=exp_file_paths
+    )
+    return []
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=8050)
