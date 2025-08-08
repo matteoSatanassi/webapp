8b13c1d989d5caf5d83b02babef87917d9e80c33
matteSata <msatanassi02@gmail.com>
2025-08-08 18:48:27 +0200

    Aggiunte le funzioni di indexer.py al Common.py

--- plotter/code/IdVd/Common.py
+++ plotter/code/IdVd/Common.py
@@ -211,6 +211,56 @@ def find_export_path()->Path:
         if export_path:
           return export_path
         i+=1
+        
+def info_extract(file_path:Path)->list:
+    info = np.array((file_path.stem.split('_'))) #es: IdVd_exponential_Vgf_2_Es_1.72_Em_1.04
+    group = f"{info[1]}_{info[7]}_{info[5]}" #file group: TrapDistr_Em_Es
+    return info[1],info[5],info[7],info[3],group,file_path # trap_distr:str, e_sigma:float, e_mid:float, v_gf:int, group:str, file_path:Path
+
+def indexer(data_dir:str|Path)->None:
+    # Params
+    data_path = Path(data_dir)
+    indexes_table_csv = data_path/"index_table.csv"
+    
+    files_list = list(Path(data_dir).glob('*.csv'))   #lista di tutti i file .csv nella data_dir
+    files_list_str = set(str(f) for f in files_list)
+    files_to_index:list[list] = []
+
+    # controlla che il file degli indici esista, altrimenti lo crea
+    if indexes_table_csv.exists():
+        df_indexes = pd.read_csv(indexes_table_csv)
+    else:
+        df_indexes = pd.DataFrame(
+            columns=[
+                'trap_distr',
+                'e_sigma',
+                'e_mid',
+                'v_gf',
+                'group',
+                'file_path'
+            ]
+        )
+
+    # rimuove le righe con file non esistenti
+    df_indexes = df_indexes[df_indexes['file_path'].isin(files_list_str)]
+
+    # add not indexed files to a list
+    indexed_files = set(df_indexes['file_path'])
+    for file in files_list:
+        if str(file) not in indexed_files:
+            if file != indexes_table_csv:
+                files_to_index.append(info_extract(file))
+
+    # aggiunge i nuovi dati a df_indexes
+    if files_to_index:
+        df_to_add = pd.DataFrame(files_to_index, columns=['trap_distr', 'e_sigma', 'e_mid', 'v_gf', 'group', 'file_path'])
+        df_indexes = pd.concat([df_indexes, df_to_add], ignore_index=True)
+
+    # salva df_indexes
+    df_indexes.to_csv(indexes_table_csv, index=False)
+
+    return None
+
 
 ## PARAMS ##
 class Config:
