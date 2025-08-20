8c486d1b4d30cd43e043797919b1d0ba16ed1e22
matteSata <msatanassi02@gmail.com>
2025-08-20 23:30:08 +0200

    Cambiato il nome alla pagina Impostata la logica con prefissi per gli id

--- plotter/code/pages/TrapDistr_page.py
+++ plotter/code/pages/TrapDistr_page.py
@@ -1,9 +1,12 @@
 import dash
 from dash import Input, Output, callback, State, dcc, html
 import dash_bootstrap_components as dbc
-from plotter.code.IdVd import *
+from plotter.code.app_elements import *
 
-dash.register_page(__name__, path='/TrapDistr-plotter')
+dash.register_page('TrapPlotter', path='/TrapData-plotter')
+
+## PARAMS ##
+PAGE_PREFIX = 'TrapData-'
 
 ## LAYOUT ##
 layout = dbc.Container(
@@ -12,7 +15,7 @@ layout = dbc.Container(
             children=[          #ogni riga ha dimensione 12 orizzontalmente
                 dbc.Col(
                     children=[
-                        my_table_template('table','TrapData')
+                        my_table_template(PAGE_PREFIX+'table','TrapData')
                     ],
                     width=4,    #larghezza colonna
                     style={'textAlign': 'center'},
@@ -20,9 +23,9 @@ layout = dbc.Container(
                 dbc.Col(
                     html.Div(
                         children=[
-                            dbc.Button("Plot ->", id="plot-button", className="me-15", color="primary"),
-                            dbc.Button("Export!", id='modal-button', className="me-15", color="primary"),
-                            export_modal('export-modal','TrapData')
+                            dbc.Button("Plot ->", id=PAGE_PREFIX+"plot-button", className="me-15", color="primary"),
+                            dbc.Button("Export!", id=PAGE_PREFIX+"modal-button", className="me-15", color="primary"),
+                            export_modal(PAGE_PREFIX+'export-modal','TrapData')
                         ],
                         style={"textAlign": "center"}
                     ),
@@ -31,9 +34,9 @@ layout = dbc.Container(
                 ),  #BOTTONI
                 dbc.Col(
                     children=[
-                        dcc.Tabs(id="tabs", value=None),
-                        html.Div(id="tabs-content"),
-                        curves_checklist('curve-checklist', 'TrapData')   # checklist curve visualizzate
+                        dcc.Tabs(id=PAGE_PREFIX+"tabs", value=None),
+                        html.Div(id=PAGE_PREFIX+"tabs-content"),
+                        curves_checklist(PAGE_PREFIX+'curve-checklist', 'TrapData')   # checklist curve visualizzate
                     ],
                     style={'textAlign': 'center'},
                     width=7     #larghezza colonna
@@ -46,3 +49,34 @@ layout = dbc.Container(
     fluid=True,
     className='TrapDistr_dashboard'
 )
+
+## CALLBACKS ##
+callback(
+    Output('tabs', 'children'),
+    Output('table','selected_rows'),
+    Output('tabs','value'),
+    Input('plot-button', 'n_clicks'),
+    State('tabs', 'derived_virtual_selected_rows'),
+    State('tabs', 'derived_virtual_data'),
+    State('tabs', 'value'),
+    State('tabs', 'children'),
+)(update_tabs_trapdata)
+
+callback(
+    Output('tabs-content', 'children'),
+    Input('tabs','value'),
+    Input('curve-checklist','value')
+)(update_graph_content)
+
+callback(
+    Output('modal-export', 'is_open'),
+    Input('modal-button', 'n_clicks'),
+    State('modal-export', 'is_open'),
+)(toggle_modal)
+callback(
+    Output('modal-export', 'is_open', allow_duplicate=True),
+    Input('close-button', 'n_clicks'),
+    State('modal-export', 'is_open'),
+    prevent_initial_call=True,
+)(toggle_modal)
+
