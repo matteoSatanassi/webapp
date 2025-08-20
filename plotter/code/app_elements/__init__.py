d0d26fcd6f4205292830ba56c4fe5564c75e4132
matteSata <msatanassi02@gmail.com>
2025-08-21 00:50:02 +0200

    tolte temporaneamente le funzioni callback

---
+++ plotter/code/app_elements/__init__.py
@@ -0,0 +1,5 @@
+# from .callback_functions import
+from .page_elements import my_table_template, mode_options, curves_checklist, export_modal
+from .parameters import data_dir
+
+__all__ = ['my_table_template', 'mode_options', 'curves_checklist', 'export_modal', 'data_dir']
