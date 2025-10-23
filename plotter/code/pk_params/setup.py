f0acec922162aa4cf56e89c944ce159b9e66ba7e
matteSata <msatanassi02@gmail.com>
2025-10-23 17:55:00 +0200

    modificato nome pacchetto parametri

--- plotter/code/pk_params/setup.py
+++ plotter/code/pk_params/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Assets_Params',                  # Nome del pacchetto
+    name='params',                  # Nome del pacchetto
     version='1.0.0',                # Versione (usa semantic versioning)
-    packages=find_packages(include=['Assets_Params', 'Assets_Params.*']),       # Trova automaticamente tutti i package
+    packages=find_packages(include=['params', 'params.*']),       # Trova automaticamente tutti i package
     description='Modulo contenente i parametri globali dell\'applicazione, indirizzi, file di configurazione e curve target',
     author='Matteo Satanassi',
 )
