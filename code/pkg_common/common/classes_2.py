19020a51da56b8da39ecbe6f334576de665b326f
matteSata <msatanassi02@gmail.com>
2025-11-17 18:19:57 +0100

    completato nuovo indexer.py

--- code/pkg_common/common/classes_2.py
+++ code/pkg_common/common/classes_2.py
@@ -3,19 +3,19 @@ import numpy as np
 import pandas as pd
 from params import *
 
-
 class FilesFeatures(object):
     def __init__(self):
         self.file_type = None
-        self._data = None
+        self._data = []
         self._grouped_by = None
 
     @classmethod
-    def from_path(cls, path:Path|str):
-        if not Path(path).exists():
-            raise FileNotFoundError(f'file {path} non trovato!')
+    def from_path(cls, file_path:Path|str):
+        if not Path(file_path).exists():
+            raise FileNotFoundError(f'file {file_path} non trovato!')
         inst = cls()
-        inst.file_type, inst._data = cls.extract_features(path)
+        inst.file_type, data = cls.extract_features(file_path)
+        inst._data.append(data)
         return inst
     @classmethod
     def from_df(cls, file_type:str, df:pd.DataFrame, grouped_by:str=None):
@@ -44,47 +44,45 @@ class FilesFeatures(object):
             raise f"Non è stato possibile convertire in indirizzi la colonna del dataframe"
 
         inst = cls()
-        inst.file_type, inst._data = file_type, df
+        inst.file_type, inst._data = file_type, df.to_dict(orient='records')
         return inst
 
     @staticmethod
-    def extract_features(path:Path|str):
+    def extract_features(file_path:Path|str):
         """
         Dato un path, controlla che il file sia di un tipo supportato,
         e in caso positivo estrae le feature contenute nel nome.
-        Ritorna la tipologia di file seguita dal df delle feature estratte.
+        Ritorna la tipologia di file seguita dal dizionario delle feature estratte.
         """
-        file_name = Path(path).stem
+        file_name = Path(file_path).stem
         file_features = file_name.split('_')
         file_type = file_features[0].upper()
         try:
             files_configs = load_files_info()[file_type]
         except:
-            raise f"Non è stato possibile processare il file {path}"
+            raise f"Non è stato possibile processare il file {file_path}"
         else:
-            df_features = pd.DataFrame([{key:None for key in files_configs["AllowedFeatures"].keys()}])
+            dict_features = {key:None for key in files_configs["AllowedFeatures"].keys()}
             for feature,f_type in files_configs["AllowedFeatures"].items():
                 if feature=='file_path':
-                    df_features[feature] = Path(path)
+                    dict_features[feature] = Path(file_path)
                 elif feature in file_features:
                     feature_val = file_features[file_features.index(feature)+1]
-                    df_features[feature] = feature_val if f_type=='text' else (
+                    dict_features[feature] = feature_val if f_type=='text' else (
                         pd.to_numeric(feature_val, downcast=f_type))
 
-            return file_type, df_features
+            return file_type, dict_features
 
     def __str__(self):
         if not self.file_type:
             return ""
         else:
-            stems = [p.stem for p in self._data["file_path"].tolist()]
+            stems = [row["file_path"].stem for row in self._data]
             return ", ".join(stems)
 
 
 if __name__ == '__main__':
-    from tabulate import tabulate
-    path = r"E:\data\IdVd_TrapDistr_exponential_Vgf_0_Es_0.2_Em_0.2.csv"
+    path = r"C:\Users\user\Documents\Uni\Tirocinio\webapp\data\IdVd_exponential_Vgf_0_Es_0.2_Em_0.2.csv"
     prova = FilesFeatures.from_path(path)
-    #print(tabulate(prova._data, headers='keys', tablefmt='psql'))
     print(prova)
 
--- code/pkg_common/common/indexer.py
+++ code/pkg_common/common/indexer.py
@@ -1,149 +1,71 @@
 from pathlib import Path
 import pandas as pd
-import numpy as np
+from classes_2 import FilesFeatures
 from params import *
-## PARAMS ##
-IDVD_COLUMNS = ('trap_distr', 'e_sigma', 'e_mid', 'v_gf', 'group', 'file_path')
-TRAPDATA_COLUMNS = ('trap_distr', 'e_sigma', 'e_mid', 'v_gf', 'start_cond', 'file_path')
 
 ## HELPER FUNC ##
-def is_valid_filename(file_path:Path)->bool:
-    """
-    Controlla che il nome del file sia supportato dall'applicazione
-    :param file_path: indirizzo del file
-    :type file_path: pathlib.Path
-    :return: True/False
-    :rtype: bool
-    """
-    return bool(fileName_pattern.match(file_path.stem))
-
-def info_extract(file_path:Path)->list:
-    """
-    Estrae le informazioni contenute nel nome di un file esperimento
-    :param file_path: indirizzo del file
-    :type file_path: pathlib.Path
-    :return: lista con i dati dell'esperimento
-    :rtype: list
-    :example:
-    >>> info_extract('../IdVd_exponential_Vgf_2_Es_1.72_Em_1.04.csv')
-    ['exponential', 1.72, 1.04, 2, 'IdVd_exponential_Em_1.04_Es_1.72', '../IdVd_exponential_Vgf_2_Es_1.72_Em_1.04.csv']
-    >>> info_extract('../TrapData_exponential_Vgf_0_Es_1.72_Em_0.18_(0,0).csv')
-    ['exponential', 1.72, 0.18, 0, 'v0', '../TrapData_exponential_Vgf_0_Es_1.72_Em_0.18_(0,0).csv']
-    """
-    info = np.array((file_path.stem.split('_'))) #es: IdVd_exponential_Vgf_2_Es_1.72_Em_1.04
-    if info[0] == 'IdVd':
-        group = f"IdVd_{info[1]}_Em_{info[7]}_Es_{info[5]}"
-        return info[1],info[5],info[7],info[3],group,str(file_path)
-    else:
-        return info[1],info[5],info[7],info[3],info[8],str(file_path)
-
-def save_indexes(df_idvd:pd.DataFrame, df_trap:pd.DataFrame, output_file:Path)->None:
-    """
-    Salva i dataframe degli indici nel file excel specificato
-    :param df_idvd: dataframe indici IdVd
-    :param df_trap: dataframe indici TrapData
-    :param output_file: indirizzo del file excel
-    :return: None
-    """
-    with pd.ExcelWriter(output_file) as writer:
-        df_idvd.to_excel(writer, sheet_name='IdVd', index=False)
-        df_trap.to_excel(writer, sheet_name='TrapData', index=False)
-
-def load_or_create_index(file_path: Path, sheet_name: str, columns: list) -> pd.DataFrame:
-    """Carica o crea un dataframe di indice"""
+def load_or_create_df_data_type(data_type:str, idx_file:Path):
     try:
-        df = pd.read_excel(file_path, sheet_name=sheet_name)
-        df['file_path'] = df['file_path'].apply(Path)   # rendo i valori della colonna dei Path invece che stringhe
-        return df
-    except (FileNotFoundError, ValueError):
-        return pd.DataFrame(columns=columns)
+        return pd.read_excel(idx_file, sheet_name=data_type)
+    except:
+        type_configs = load_files_info()[data_type]
+        columns = [key for key in type_configs["AllowedFeatures"].keys()]
+        return add_aff_cols(data_type, pd.DataFrame(columns=columns))
 
-def process_new_file(file: Path) -> tuple:
-    """
-    Elabora un singolo file
-    :param file: indirizzo del file
-    :type file: pathlib.Path
-    :return: tuple
-    """
-    # controlla che il nome del file sia supportato
-    if not is_valid_filename(file):
-        return None, None
-
-    data = info_extract(file)
-    return 'IdVd' if 'IdVd' in file.name else 'TrapData', data
-
-def update_dataframe(df:pd.DataFrame, rows_to_add:list, columns:list) -> pd.DataFrame:
-    """
-    Concatena un df con una lista di righe
-    :param df: dataframe a cui concatenare le nuove righe
-    :param rows_to_add: lista di righe da aggiungere
-    :param columns: lista di colonne del df
-    :return: il df con le nuove righe in fondo
-    """
-    df_to_concat = pd.DataFrame(rows_to_add, columns=columns)
-    return pd.concat([df, df_to_concat], ignore_index=True)
+def add_aff_cols(data_type:str, df:pd.DataFrame):
+    type_configs = load_files_info()[data_type]
+    if type_configs["TargetCurves"]==1:
+        for curve in type_configs["AllowedCurves"]:
+            df[f"aff_{curve}"] = None
+    return df
 
 ## MAIN FUNC ##
-def indexer(data_dir: str | Path) -> list[Path]:
-    """
-    Data una cartella di file .csv di dati crea un file indice utile al funzionamento dell'applicazione
-    :param data_dir: indirizzo della directory da indicizzare
-    :type data_dir: pathlib.Path
-    :return: lista di file con nomi non supportati
-    :rtype: list[Path]
-    """
-    # Params
+def indexer(data_dir:str|Path)->list[Path]:
+    # config params
+    files_configs = load_files_info()
+
+    # params
     data_dir = Path(data_dir)
     excel_indexes_file = data_dir / "indexes.xlsx"
 
-    files_set = set(data_dir.glob('*.csv'))  # set di tutti i file .csv nella data_dir
-    files_df = pd.DataFrame([{
-        "file_type": file.stem.split('_')[0].upper(), "file_path": str(file),
-    } for file in files_set])
+    files_set = set(data_dir.glob("*.csv"))
+    df_files = pd.DataFrame([
+        {"file_type": f.stem.split("_")[0].upper(), "file_path": str(f)}
+        for f in files_set
+    ])
 
-    files_to_index = {'IdVd':[],'TrapData':[]}
-    unsupported_files: list[Path] = []
+    # cache dict to save df before writing them
+    df_to_save = {}
 
-    for FileType in load_files_info().keys():
+    # processing one supported data_type per loop
+    for data_type, type_configs in files_configs.items():
 
-    # controlla che il file degli indici esista, altrimenti crea il DataFrame
-    df_indexes_IdVd = load_or_create_index(excel_indexes_file, 'IdVd', IDVD_COLUMNS)
-    df_indexes_TrapData = load_or_create_index(excel_indexes_file, 'TrapData', TRAPDATA_COLUMNS)
+        if data_type not in df_files["file_type"].tolist():
+            continue
 
-    # rimuove le righe con file non esistenti
-    df_indexes_IdVd = df_indexes_IdVd[df_indexes_IdVd['file_path'].isin(files_set)]
-    df_indexes_TrapData = df_indexes_TrapData[df_indexes_TrapData['file_path'].isin(files_set)]
+        # controlla che esista il file degli indici e in caso negativo crea un df vuoto
+        df_data_type = load_or_create_df_data_type(data_type, excel_indexes_file)
 
-    # add not indexed files to a list
-    indexed_files = set(df_indexes_IdVd['file_path']) | set(df_indexes_TrapData['file_path'])
-    for file in files_set:
-        if file not in indexed_files:
-            file_type, file_data = process_new_file(file)
-            if file_data:
-                files_to_index[file_type].append(file_data)
-            else:
-                unsupported_files.append(file)
+        # rimuove le righe con file non esistenti
+        df_data_type = df_data_type[df_data_type['file_path'].isin(files_set)]
 
-    # aggiunge i nuovi dati a df_indexes
-    if files_to_index['IdVd']:
-        df_indexes_IdVd = update_dataframe(df_indexes_IdVd, files_to_index['IdVd'], IDVD_COLUMNS)
-    if files_to_index['TrapData']:
-        df_indexes_TrapData = update_dataframe(df_indexes_TrapData, files_to_index['TrapData'], TRAPDATA_COLUMNS)
+        # add not indexed files to a list
+        indexed_files = set(df_data_type['file_path'])
+        rows = []   # vi aggiungerò le features dei file non indicizzati
+        for f in df_files.loc[df_files["file_type"]==data_type]["file_path"]:
+            if f not in indexed_files:
+                rows.append(FilesFeatures.extract_features(f)[1])
 
-    # salva df_indexes
-    save_indexes(df_indexes_IdVd, df_indexes_TrapData, excel_indexes_file)
+        if rows:
+            df_to_concat = add_aff_cols(data_type, pd.DataFrame(data=rows))
 
-    # stampa un log dei file con nomi non supportati se ce ne sono
-    if unsupported_files:
-        print("Files non indicizzati:\n" + "\n".join(map(str, unsupported_files)))
+            df_to_save[data_type] = df_to_concat if df_data_type.empty else pd.concat([df_data_type, df_to_concat])
 
-    return unsupported_files
+    # salvo i df alla fine del processo
+    with pd.ExcelWriter(excel_indexes_file) as writer:
+        for key, df in df_to_save.items():
+            df.to_excel(writer, sheet_name=key, index=False)
 
-fileName_pattern = re.compile(
-    r"^(IdVd|TrapData)_"            # inizia con IdVd_ o TrapData_
-    r"(exponential|gaussian|uniform|level)_"  # distribuzione
-    r"Vgf_-?\d+_"                   # Vgf_ seguito da intero (può essere negativo)
-    r"Es_\d+(\.\d+)?_"               # Es_ seguito da numero (con o senza decimali)
-    r"Em_\d+(\.\d+)?"                # Em_ seguito da numero (con o senza decimali)
-    r"(_\(-?\d+,-?\d+\))?$"              # opzionale _(x,y) per TrapData
-)
+if __name__ == "__main__":
+    from params import data_dir
+    indexer(data_dir)
